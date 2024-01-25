940e5d83f72b7c1980cb0a04a9c0282c8f2e2cdf
AnnyMasovskaya <kadzhaevaa@mail.ru>
2024-01-25 18:36:35 +0300

    edit1

---
+++ src/components/Snackbar/SnackBarCart.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Snackbar, { SnackbarOrigin } from "@mui/material/Snackbar";
+
+interface State extends SnackbarOrigin {
+  open: boolean;
+}
+
+export default function PositionedSnackbar() {
+  const [state, setState] = React.useState<State>({
+    open: false,
+    vertical: "top",
+    horizontal: "center",
+  });
+  const { vertical, horizontal, open } = state;
+
+  const createHandleClick = (newState: SnackbarOrigin) => () => {
+    setState({ ...newState, open: true });
+  };
+
+  const handleClose = () => {
+    setState({ ...state, open: false });
+  };
+
+  const buttons = (
+    <React.Fragment>
+      <Grid item xs={6} textAlign="right">
+        <Button
+          onClick={createHandleClick({
+            vertical: "bottom",
+            horizontal: "right",
+          })}
+        />
+      </Grid>
+    </React.Fragment>
+  );
+
+  return (
+    <Box sx={{ width: 500 }}>
+      {buttons}
+      <Snackbar
+        anchorOrigin={{ vertical, horizontal }}
+        open={open}
+        onClose={handleClose}
+        message={""}
+        key={vertical + horizontal}
+      />
+    </Box>
+  );
+}
